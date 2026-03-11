6610da5aa900ad5728411ba745156ffc0cfe4b16
sjvenditto <sjvenditto@gmail.com>
2026-03-11 09:25:13 +0000

    add docs and workflow (workflow disabled)

---
+++ docs/conf.py
@@ -0,0 +1,53 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "PreCosyne BrainHack 2026"
+copyright = "2026, Pynapple team"
+author = "Pynapple team"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx_design",
+    "myst_nb",
+    "sphinx_copybutton",
+    # "sphinx_togglebutton",
+]
+
+myst_enable_extensions = [
+    "colon_fence",
+    "dollarmath",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+nb_execution_timeout = -1
+nb_execution_raise_on_error = True
+myst_enable_extensions = ["colon_fence", "dollarmath", "attrs_inline"]
+nb_execution_mode = "cache"
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "pydata_sphinx_theme"
+html_static_path = ["_static"]
+html_css_files = ["mycss.css"]
+# html_js_files = ["myjs.js"]
+
+html_theme_options = {
+    "logo": {
+        "text": [],
+    },
+    "secondary_sidebar_items": {
+        "**": ["page-toc"],
+        "index": [],
+    },
+    "navbar_persistent": [],
+}
---
+++ docs/index.md
@@ -0,0 +1,26 @@
+---
+jupytext:
+  text_representation:
+    extension: .md
+    format_name: myst
+    format_version: 0.13
+    jupytext_version: 1.18.1
+kernelspec:
+  name: python3
+  display_name: Python 3 (ipykernel)
+  language: python
+orphan: true
+---
+
+# PreCosyne BrainHack 2026
+
+
+```{code-cell} ipython3
+%matplotlib inline
+%load_ext autoreload
+%autoreload 2
+import warnings
+
+```
+
+
